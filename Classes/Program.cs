b60345fd7a5b332d17ef4b288b7dcc357d328f2f
Hiaguedes <hiago@contasimples.com>
2021-11-06 01:00:41 -0300

    comecando com classes no c#

---
+++ Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta1 = new ContaCorrente
+            {
+                Agencia = 1235,
+                Numero = 1,
+                Saldo = 120,
+                Titular = "Gabriela Fernandes"
+            };
+
+            Console.WriteLine(conta1.Titular);
+
+            conta1.Saldo += 500;
+            Console.WriteLine(conta1.Saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- LacosDeRepeticao/Program.cs
+++ LacosDeRepeticao/Program.cs
@@ -10,6 +10,21 @@ namespace LacosDeRepeticao
     {
         static void Main(string[] args)
         {
+
+            double valorInvestido = 1000;
+
+            for (int mes = 1; mes<13; mes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + mes + (mes > 1 ? " meses" : " mês") +  " o valor total será de " + valorInvestido);
+
+            }
+
+
+            Console.WriteLine(valorInvestido);
+
+
+            Console.ReadLine();
         }
     }
 }
