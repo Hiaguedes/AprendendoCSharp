4cb4e128fc023d2e75df0814c7cba3ad4ac8d894
Hiaguedes <hiagorguedes@gmail.com>
2021-12-03 00:24:11 -0300

    chore: fazendo lista generica no csharp

---
+++ Objetos/Lista.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Lista
+{
+    public class Lista<T>
+    {
+        private T[] _listItems { get; set; }
+        private int _indiceVago { get; set; } = 0 ;
+        private int _listLenght { get; set; }
+        public int Length { get { return _indiceVago; } }
+        public Lista(int initialLenght = 5)
+        {
+            _listLenght = initialLenght;
+            _listItems = new T[_listLenght];
+        }
+
+        public void Push(T item)
+        {
+            _VerificaNecessidadeAumentarArray();
+            _listItems[_indiceVago] = item;
+            _indiceVago++;
+        }
+
+        public void Log() {
+            Console.WriteLine(String.Join('-', _listItems));
+        }
+
+        public T Peek() { return _listItems[_indiceVago];}
+
+        private void _ChangeArrayLenght(int lenght)
+        {
+            T[] newArray = new T[lenght];
+            _listLenght = lenght;
+            _listItems.CopyTo(newArray, 0);
+            _listItems = newArray;
+        }
+
+        private void _VerificaNecessidadeAumentarArray()
+        {
+            if (_listItems.Length == _indiceVago + 1)
+            {
+                _ChangeArrayLenght(2 * _listLenght);
+            }
+        }
+
+        public void Pop()
+        {
+            _VerificaNecessidadeAumentarArray();
+            _listItems[_indiceVago] = _listItems[_indiceVago + 1];
+            _indiceVago--;
+        }
+
+        public void Remove(int indexToRemove)
+        {
+            if(indexToRemove < 0 || indexToRemove > _listItems.Length)
+            {
+                throw new IndexOutOfRangeException("Indicie deve ser maior ou igual a zero e estar dentro do tamanho do array");
+            }
+
+            for(int i = indexToRemove; i < _listItems.Length - 1; i++)
+            {
+                _listItems[i] = _listItems[i + 1];
+            }
+            _indiceVago--;
+        }
+
+        public void Remove(T item)
+        {
+            if(item == null)
+            {
+                throw new ArgumentException("Item nao pode ser nulo");
+            }
+            int indexToRemove = -1;
+            for(int i = 0; i <_listItems.Length; i++)
+            {
+                if(item.Equals(_listItems[i]))
+                {
+                    indexToRemove = i;
+                    break;
+                }
+            }
+
+            if (indexToRemove < 0) throw new ArgumentOutOfRangeException($"Argumento {nameof(item)} nao esta presente nos items criados");
+
+            Remove(indexToRemove);
+        }
+
+        public void PushMany(params T[] items)
+        {
+            foreach(T item in items)
+            {
+                Push(item);
+            }
+        }
+
+        public T this[int indice] // pega a referencia do _listItem do lado de fora da classe
+        {
+            get { return _listItems[indice]; }
+        } 
+    }
+}
+
+/*
+ public [__] this[__]
+{
+    get
+    {
+        ContaCorrente[] resultado = new ContaCorrente[__];
+        for (int i = 0; i < indices.Length; i++)
+        {
+            int indiceDaLista = indices[i];
+            resultado[i] = GetItemNoIndice(indiceDaLista);
+        }
+        return resultado;
+    }
+}
+
+Resultado: 
+
+ContaCorrente[], [params int[] indices] e [indices.Length].
+ */
--- Objetos/Program.cs
+++ Objetos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ByteBank.Funcionarios;
+using System.Collections;
+using Objetos.Lista;
 
 Console.WriteLine("Resultado to String " + new Desenvolvedor("111111").ToString());
 
@@ -6,3 +8,73 @@ object dev1 = new Desenvolvedor("111111");
 object dev2 = new Desenvolvedor("111111");
 
 Console.WriteLine(dev1.Equals(dev2));
+
+int[] idades = new int[5];
+
+idades[0] = 10;
+idades[1] = 35;
+idades[2] = 27;
+idades[3] = 8;
+idades[4] = 41;
+Console.WriteLine(String.Join('-', idades));
+
+string[] listaComprar = new string[5];
+
+listaComprar[2] = "dwdwdwd";
+
+Console.WriteLine(String.Join('-', listaComprar));
+
+int acc = 0;
+for (int i = 0; i < idades.Length; i++)
+{
+    int idade = idades[i];
+    //Console.Beep();
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine($"Idade coloca no indice {i} e de valor {idade}");
+    acc += idade;
+}
+
+Console.WriteLine($"Acumulador = {acc}");
+Console.WriteLine($"Media = {acc / idades.Length}");
+
+Desenvolvedor[] devs = new Desenvolvedor[5];
+
+devs[0] = new Desenvolvedor("1111");
+
+Console.WriteLine(devs[0].GetType());
+//Console.WriteLine(devs[1].GetType()); //erro null exception
+
+
+int[] novasIdades = new int[] { 1, 2, 39, 32 };
+
+Console.WriteLine(String.Join("-", novasIdades));
+
+Console.WriteLine(value: "aa");
+
+
+string[] Verdades = new string[] { "O", "Mateus", "Valente", "É", "Uma", "Bicha" };
+
+for(int i = 0; i < Verdades.Length; i++)
+{
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine(Verdades[i]);
+    //Console.Beep();
+}
+
+var lista = new Lista<string>();
+
+lista.Push("Oi");
+lista.Push("Koe");
+lista.Push("Bele");
+lista.Push("Oi1");
+lista.Push("Koe1");
+lista.Push("Bele1");
+lista.PushMany(new string[] {"A", "B", "C", "D"});
+lista.PushMany("A1", "B1", "C1"); // o params cria um array se nao existe um array com a tipagem necessaria
+lista.Remove(2);
+lista.Log();
+
+Console.WriteLine(lista[1]);
+Console.WriteLine(lista.Length);
